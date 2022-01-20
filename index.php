823ef5e7bfdbc89cfb7db5c51790e798990419c1
Max8583 <vlasov-maksim@mail.ru>
2022-01-20 18:00:31 +0300

    изменение composer.json index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hexlet one love.
+zzzzz
+
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
